cd9a7eff8130c8cf531e391e1bfca1abcf871ff4
PauloFilho197 <paulofilho.dev@gmail.com>
2025-08-11 12:53:24 -0300

    feature/dashboard-responsavel

---
+++ frontend/src/dashboard/responsaveis/perfil-responsavel/perfil-responsavel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-responsavel',
+  imports: [],
+  templateUrl: './perfil-responsavel.component.html',
+  styleUrl: './perfil-responsavel.component.scss'
+})
+export class PerfilResponsavelComponent {
+
+}
