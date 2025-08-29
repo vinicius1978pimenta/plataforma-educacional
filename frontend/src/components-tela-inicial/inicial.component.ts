0a44e6896ef6d09f9e9ae3605c5cba8c3f7c8c09
PauloFilho197 <paulofilho.dev@gmail.com>
2025-08-28 22:41:26 -0300

    fix/ajustes footer

--- frontend/src/components-tela-inicial/inicial.component.ts
+++ frontend/src/components-tela-inicial/inicial.component.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CarroselComponent } from './carrosel-fotos/carrosel/carrosel.component';
-import { SobreNosComponent } from './sobre-nos-card/sobre-nos/sobre-nos.component';
 import { ConteudoComponent } from './conteudo-principal/conteudo/conteudo.component';
 import { Conteudo2Component } from './conteudo-2/conteudo-2/conteudo-2.component';
 import { Conteudo3Component } from './conteudo-3/conteudo-3/conteudo-3.component';
@@ -17,7 +16,6 @@ import { FooterComponent } from './footer/footer.component';
     CommonModule,
     NavbarComponent,
     CarroselComponent,
-    SobreNosComponent,
     ConteudoComponent,
     Conteudo2Component,
     Conteudo3Component,
