6b94149059311102f198c6c0f9afdaf32c67d88c
VitorLourenco28 <vitormoura28072@gmail.com>
2025-09-01 13:39:19 -0300

    frontend/fix/ajustes

--- frontend/src/components-tela-inicial/footer/footer.component.ts
+++ frontend/src/components-tela-inicial/footer/footer.component.ts
@@ -10,7 +10,7 @@ import { RouterModule } from '@angular/router';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent2 {
   currentYear = new Date().getFullYear();
   email = 'contato@interlinguas.com';
 
