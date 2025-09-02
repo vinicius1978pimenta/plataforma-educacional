d64f1d98bcb6a3bd638afb7708c987b02d815b6b
vinicius1978pimenta <144499204+vinicius1978pimenta@users.noreply.github.com>
2025-09-02 10:08:07 -0300

    frontend/traducao/dashboar-aluno

--- frontend/src/dashboard/aluno/navbar/navbar.component.ts
+++ frontend/src/dashboard/aluno/navbar/navbar.component.ts
@@ -1,28 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { Router, RouterModule } from '@angular/router';
+import { TranslocoModule, TranslocoPipe, TranslocoService } from '@ngneat/transloco';
 
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  imports: [RouterModule,TranslocoModule,TranslocoPipe],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent implements OnInit{
+  botoes: any
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private transloco: TranslocoService
   ) {}
+
+  changeLanguage(lang: string) {
+    this.transloco.setActiveLang(lang);
+      }
+    
+  getCurrentLang() {
+    return this.transloco.getActiveLang();
+  }
 
   currentUser: any;
   ngOnInit(): void {
-    const userData = localStorage.getItem('user');
-    if (userData) {
-      this.currentUser = JSON.parse(userData);
-    };
+      const userData = localStorage.getItem('user');
+      if (userData) {
+        this.currentUser = JSON.parse(userData);
+      };
+
+      this.changeLanguage
+      this.getCurrentLang
     }
+
+
   
   logout() {
       const sair = confirm("deseja realmente sair ?")
