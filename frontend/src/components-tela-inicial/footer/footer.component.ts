8138f45a374c446aca7bd6c13b2d4b883471e8af
PauloFilho197 <paulofilho.dev@gmail.com>
2025-09-09 14:25:30 -0300

    traducao footer

--- frontend/src/components-tela-inicial/footer/footer.component.ts
+++ frontend/src/components-tela-inicial/footer/footer.component.ts
@@ -1,12 +1,13 @@
 import { Component, Inject } from '@angular/core';
 import { DOCUMENT, CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { TranslocoModule, TranslocoService } from '@ngneat/transloco';
 
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, TranslocoModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
@@ -14,7 +15,23 @@ export class FooterComponent2 {
   currentYear = new Date().getFullYear();
   email = 'contato@interlinguas.com';
 
-  constructor(@Inject(DOCUMENT) private document: Document) {}
+  constructor(@Inject(DOCUMENT) private document: Document,
+  private translocoService: TranslocoService
+
+
+) {}
+
+
+
+          changeLanguage(lang: string) {
+            this.translocoService.setActiveLang(lang);
+          }
+        
+          getCurrentLang() {
+            return this.translocoService.getActiveLang();
+          }
+
+
 
   backToTop() {
     this.document.documentElement.scrollTo({ top: 0, behavior: 'smooth' });
